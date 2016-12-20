51ec1041eafa163d330637675daea90980109eae
Ichwan Palongengi <ich9681@gmail.com>
2016-12-19 20:38:18 -0500

    Remove frame classes and manually instantiate all widgets

--- CharacterCounterInterface.py
+++ CharacterCounterInterface.py
@@ -1,47 +1,27 @@
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+import CharacterCounterLogic as makePiechart
 
-class WidgetLabel(Frame):
-    def __init__(self,parent):
-        Frame.__init__(self, parent)
-        lblWidget = Label (self, text = "CharacterCounter v1.0")
-        lblWidget.pack (side = TOP, fill = BOTH)
-
-class FileFinder(Frame):
-    def __init__ (self, parent):
-        Frame.__init__(self, parent)
-        self.dirname = ""
-        self.lblFileFinder = Label (self, text = "File Name:", width = 15, anchor = W)
-        self.lblFileFinder.pack (side = LEFT)
-        Entry(self).pack(side = LEFT)
-        self.btnFileFinder = Button (self, text = "Browse", width = 10, command = self.buttonClicked)
-        self.btnFileFinder.pack (side = RIGHT, padx=3)
-
-    def buttonClicked(self):
-        self.dirname = askopenfilename(title="Select a text file", filetypes=(("text files", "*.txt"), ("all files", "*.*")))
-        print(self.dirname)
-
-class SlicesNumber(Frame):
-    def __init__(self,parent):
-        Frame.__init__(self, parent)
-        self.lblSlicesNumber = Label (self, text = "# of Slices:", width = 15, anchor = W)
-        self.lblSlicesNumber.pack (side = LEFT)
-        Entry(self, width = 5).pack(side = LEFT)
-        self.lblSlicesInfo = Label(self, text="Limit to 20", width = 15)
-        self.lblSlicesInfo.pack(side=LEFT)
-
+def buttonClicked():
+    global numOfSlices
+    makePiechart.drawPieChart('Words.txt', numOfSlices.get())
 
 root = Tk()
 root.wm_title("Character Counter v1.0")
+numOfSlices = IntVar()
 
-title = WidgetLabel(root)
-title.pack(fill = X, padx = 3)
+titleFrame = Frame (root)
+lblWidget = Label (titleFrame, text = "Character Counter v1.0")
+lblWidget.pack (side = TOP, fill = BOTH)
+titleFrame.pack(fill = X, padx = 3)
 
-blah = FileFinder (root)
-blah.pack(fill = X, padx = 3)
+numOfSlicesFrame = Frame (root)
+lblSlicesNumber = Label (numOfSlicesFrame, text = "# of Slices:", width = 15, anchor = W)
+lblSlicesNumber.pack (side = LEFT)
+txtnumOfSlices = Entry(numOfSlicesFrame, width = 5, textvariable = numOfSlices).pack(side = LEFT)
+lblSlicesInfo = Label(numOfSlicesFrame, text="Limit to 20", width = 15)
+lblSlicesInfo.pack(side=LEFT)
+numOfSlicesFrame.pack(fill = X, padx = 3)
 
-zap = SlicesNumber (root)
-zap.pack(fill = X, padx = 3)
-
-Button(root, text = "Execute", width = 10).pack (side = RIGHT, anchor = SE, padx = 6)
+executeButton = Button(root, text = "Execute", width = 10, command = buttonClicked)
+executeButton.pack (side = RIGHT, anchor = SE, padx = 6)
 root.mainloop()
